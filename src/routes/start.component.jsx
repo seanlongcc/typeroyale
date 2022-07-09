32d40b42f65fe72a9e30aa5cac86b6ebcf0d3248
Sean CC <seanlongcc@gmail.com>
2022-07-09 17:39:42 -0400

    added links for home and practice

--- src/routes/start.component.jsx
+++ src/routes/start.component.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Start = () => {
 	return (
 		<div className='grid grid-rows-3 items-center justify-center h-screen'>
 			<h1 className='text-3xl'>Start</h1>
-			<button className='text-1xl'>Practice</button>
+			<Link className='text-1xl' to='/practice'>
+				Practice
+			</Link>
 			<button className='text-1xl'>Multiplayer</button>
 			<Outlet />
 		</div>
--- src/routes/static-elements.component.jsx
+++ src/routes/static-elements.component.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const StaticElements = () => {
 	return (
 		<div>
-			<button className='absolute left-0 text-4xl font-bold p-5'>
+			<Link className='absolute left-0 text-4xl font-bold p-5' to='/'>
 				TypeRoyal
-			</button>
+			</Link>
 			<button className='absolute bottom-0 p-5'>Settings</button>
 			<h1 className='absolute bottom-0 right-0 p-5'>Version 1.0.0</h1>
 			<button className='absolute right-0 p-5'>Log In</button>
