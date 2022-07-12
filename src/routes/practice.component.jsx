62ef7890300b5a309d35c932190f4bf7f77ae882
Sean CC <seanlongcc@gmail.com>
2022-07-12 04:54:20 -0400

    updated static padding

--- src/routes/practice.component.jsx
+++ src/routes/practice.component.jsx
@@ -1,18 +1,22 @@
-import PracticeModes from "../components/practice/practice-modes.component";
-import Clock from "../components/clock/clock.component";
-import { useState } from "react";
+import PracticeModes from '../components/practice/practice-modes.component';
+import Clock from '../components/clock/clock.component';
+import { useState } from 'react';
 
 const Practice = () => {
-  const [ready, setReady] = useState(false);
+	const [ready, setReady] = useState(false);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <span className="grid gap-y-52 text-5xl">{ready && <Clock />}</span>
-      <span className="grid text-3xl">PRACTICE TEXT HERE</span>
-      <PracticeModes />
-      <button onClick={() => setReady(!ready)}>start</button>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center justify-center h-screen'>
+			{/* conditional rendering: inline If with logical && operator, runs Clock if ready is true */}
+			<span className='text-5xl'>{ready && <Clock />}</span>
+			<span className='text-3xl'>PRACTICE TEXT HERE</span>
+			<span className=''>
+				<PracticeModes />
+			</span>
+			{/* button sets ready to true which starts the timer */}
+			<button onClick={() => setReady(!ready)}>start</button>
+		</div>
+	);
 };
 
 export default Practice;
