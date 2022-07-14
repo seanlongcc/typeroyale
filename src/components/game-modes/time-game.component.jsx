2682ad4bb586bc849d5be28ca59fddcac3379af7
Sean CC <seanlongcc@gmail.com>
2022-07-14 00:43:25 -0400

    cleaned up variables

--- src/components/game-modes/time-game.component.jsx
+++ src/components/game-modes/time-game.component.jsx
@@ -1,39 +1,38 @@
-import { useState, useEffect } from "react";
-import Button from "../button/button.component";
-import ClockDown from "../clock-down/clock-down.component";
-import TextBox from "../text-box/text-box.component";
-
-const validChars =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,/'\"!?@#$%^&*()_+-=<>\\|`~[]{};: ";
+import { useState } from 'react';
+import Button from '../button/button.component';
+import ClockDown from '../clock-down/clock-down.component';
+import TextBox from '../text-box/text-box.component';
 
 const TimeGame = () => {
-  const [mode, setMode] = useState("60");
-  const [ready, setReady] = useState(false);
+	const [mode, setMode] = useState('60');
+	const [ready, setReady] = useState(false);
 
-  // 
-  const [typed, setTyped] = useState("");
+	//
+	const [typed, setTyped] = useState('');
 
-  return (
-    <div>
-      <span
-        className={ready ? "hidden" : "flex flex-col items-center text-5xl"}
-      >
-        {mode}
-      </span>
-      <span className="flex flex-col items-center text-5xl">
-        {ready && <ClockDown gameTime={mode} />}
-      </span>
-      <span>{<TextBox typed={typed} setTyped={setTyped} setReady={setReady}/>}</span>
-      {/* conditional rendering: inline If with logical && operator: run Clock if ready is true */}
-      <span className="grid grid-cols-5">
-        <Button label={"15"} mode={mode} setMode={setMode} />
-        <Button label={"30"} mode={mode} setMode={setMode} />
-        <Button label={"60"} mode={mode} setMode={setMode} />
-        <Button label={"120"} mode={mode} setMode={setMode} />
-        <Button label={"custom"} mode={mode} setMode={setMode} />
-      </span>
-    </div>
-  );
+	return (
+		<div>
+			<span
+				className={ready ? 'hidden' : 'flex flex-col items-center text-5xl'}
+			>
+				{mode}
+			</span>
+			<span className='flex flex-col items-center text-5xl'>
+				{ready && <ClockDown gameTime={mode} />}
+			</span>
+			<span>
+				{<TextBox typed={typed} setTyped={setTyped} setReady={setReady} />}
+			</span>
+			{/* conditional rendering: inline If with logical && operator: run Clock if ready is true */}
+			<span className='grid grid-cols-5'>
+				<Button label={'15'} mode={mode} setMode={setMode} />
+				<Button label={'30'} mode={mode} setMode={setMode} />
+				<Button label={'60'} mode={mode} setMode={setMode} />
+				<Button label={'120'} mode={mode} setMode={setMode} />
+				<Button label={'custom'} mode={mode} setMode={setMode} />
+			</span>
+		</div>
+	);
 };
 
 export default TimeGame;
--- src/components/text-box/text-box.component.jsx
+++ src/components/text-box/text-box.component.jsx
@@ -1,55 +1,53 @@
-import { useState, useEffect } from "react";
-
 const validChars =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,/'\"!?@#$%^&*()_+-=<>\\|`~[]{};: ";
+	'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,/\'"!?@#$%^&*()_+-=<>\\|`~[]{};: ';
+const validCharSet = new Set(validChars.split(''));
 
-const TextBox = ({typed, setTyped, setReady}) => {
-  //placeholder text for now
-  let passage = "the quick brown fox jumps";
+const TextBox = ({ typed, setTyped, setReady }) => {
+	//placeholder text for now
+	let passage = 'the quick brown fox jumps';
 
-  document.onkeydown = (e) => {
+	document.onkeydown = (e) => {
+		if (e.key === 'Backspace') {
+			if (typed.length > 0) {
+				setTyped((typed) => typed.slice(0, -1));
+			}
+		} else if (
+			validCharSet.has(e.key) &&
+			//limits the maximum length of typed characters, -1 since it renders first
+			typed.length <= passage.length - 1
+		) {
+			if (typed.length === 0) {
+				setReady(true);
+			}
+			setTyped((typed) => typed + e.key);
+		}
+	};
 
-    if (e.key === "Backspace") {
-      if (typed.length > 0) {
-        setTyped((typed) => typed.slice(0, -1));
-      }
-    } else if (
-      validChars.includes(e.key) &&
-      //limits the maximum length of typed characters, -1 since it renders first
-      typed.length <= passage.length - 1
-    ) {
-      if (typed.length === 0) {
-        setReady(true);
-      }
-      setTyped((typed) => typed + e.key);
-    }
-  };
-
-  return (
-    <div className="text-3xl">
-      <span>
-        {/* splits passage into array of single characters and maps each character to an index */}
-        {passage.split("").map((c, i) => {
-          if (typed[i] === c) {
-            return (
-              <span key={i} className="text-green-500">
-                {c}
-              </span>
-            );
-          } else if (typed[i]) {
-            return (
-              <span key={i} className="text-red-500 bg-transparent bg-red-100">
-                {c}
-              </span>
-            );
-          } else {
-            //returns the passage if nothing is typed, renders all at once
-            return <span key={i}>{c}</span>;
-          }
-        })}
-      </span>
-    </div>
-  );
+	return (
+		<div className='text-3xl'>
+			<span>
+				{/* splits passage into array of single characters and maps each character to an index */}
+				{passage.split('').map((c, i) => {
+					if (typed[i] === c) {
+						return (
+							<span key={i} className='text-green-500'>
+								{c}
+							</span>
+						);
+					} else if (typed[i]) {
+						return (
+							<span key={i} className='text-red-500 bg-transparent bg-red-100'>
+								{c}
+							</span>
+						);
+					} else {
+						//returns the passage if nothing is typed, renders all at once
+						return <span key={i}>{c}</span>;
+					}
+				})}
+			</span>
+		</div>
+	);
 };
 
 export default TextBox;
