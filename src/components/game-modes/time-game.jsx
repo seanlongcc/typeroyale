3b184cc0902ddbce45ce00089ee1961bd5c7920a
rahil <rahil@vt.edu>
2022-07-23 19:38:03 -0400

    fixed text wrap issue. needs to be fully tested.

--- src/components/game-modes/time-game.jsx
+++ src/components/game-modes/time-game.jsx
@@ -5,9 +5,7 @@ import GameStats from "../game-stats/game-stats";
 import TextBox from "../text-box/text-box";
 
 // temporary
-let passage = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+let passage = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
 const TimeGame = ({ typed, setTyped, ready, setReady }) => {
   const [mode, setMode] = useState(60);
