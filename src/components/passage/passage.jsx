088f74e805691f53f63a85eaaf0dd75f98189fe8
TimTran01 <ttran35112@gmail.com>
2022-07-24 14:56:21 -0400

    passage component

--- src/components/passage/passage.jsx
+++ src/components/passage/passage.jsx
@@ -1,20 +1,20 @@
-import ENGLISH_200 from '../../assets/word-lists/english-200.json';
-import ENGLISH_1k from '../../assets/word-lists/english-1000.json';
+import ENGLISH_200 from "../../assets/word-lists/english-200.json";
+import ENGLISH_1k from "../../assets/word-lists/english-1000.json";
 
 const Passage = ({ wordCount, chooseList }) => {
-	//temp
-	wordCount = 100;
-	chooseList = ENGLISH_200.words;
-	const wordList = [];
-	let passage = '';
+  //temp
+  wordCount = 100;
+  chooseList = ENGLISH_200.words;
+  const wordList = [];
+  let passage = "";
 
-	for (let i = 1; i <= wordCount; i++) {
-		const word = chooseList[Math.floor(Math.random() * 200)];
-		wordList.push(word);
-		passage = wordList.join(' ');
-	}
+  for (let i = 1; i <= wordCount; i++) {
+    const word = chooseList[Math.floor(Math.random() * 200)];
+    wordList.push(word);
+    passage = wordList.join(" ");
+  }
 
-	return <span>{passage}</span>;
+  return <span>{passage}</span>;
 };
 
 export default Passage;
