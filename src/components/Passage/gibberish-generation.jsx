4b8832cf62ea7c4e95ac6f2e37bd2d0426619d08
Sean CC <seanlongcc@gmail.com>
2022-07-27 20:29:33 -0400

    moved code in gibberish-generation

--- src/components/Passage/gibberish-generation.jsx
+++ src/components/Passage/gibberish-generation.jsx
@@ -1,21 +1,20 @@
 const validChars =
 	'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,/\'"!?@#$%^&*()_+-=<>\\|`~[]{};:';
 
-const generateString = (wordCount) => {
-	let wordArray = [];
-
-	for (let i = 0; i < wordCount; i++) {
-		let str = '';
-		let gibLength = Math.floor(Math.random() * 10) + 1;
-		for (let j = 0; j < gibLength; j++) {
-			str += validChars.charAt(Math.floor(Math.random() * validChars.length));
-		}
-		wordArray.push(str);
-	}
-	return wordArray;
-};
-
 export const generateGibberish = (wordCount) => {
+	const generateString = (wordCount) => {
+		let wordArray = [];
+
+		for (let i = 0; i < wordCount; i++) {
+			let str = '';
+			let gibLength = Math.floor(Math.random() * 10) + 1;
+			for (let j = 0; j < gibLength; j++) {
+				str += validChars.charAt(Math.floor(Math.random() * validChars.length));
+			}
+			wordArray.push(str);
+		}
+		return wordArray;
+	};
 	const gibPassage = generateString(wordCount);
 	const LINE_LEN = 50; // play around with value depending on desired text box size
 
