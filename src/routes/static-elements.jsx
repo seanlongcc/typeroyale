fe40258f7e95cf177309d3c99723226857db197d
Sean CC <seanlongcc@gmail.com>
2022-07-31 19:48:49 -0400

    lightmode toggle

--- src/routes/static-elements.jsx
+++ src/routes/static-elements.jsx
@@ -1,8 +1,20 @@
 import { Outlet, Link } from 'react-router-dom';
-import { RiSettings3Line, RiGithubLine, RiUser3Line } from 'react-icons/ri';
+import {
+	RiSettings3Line,
+	RiGithubLine,
+	RiUser3Line,
+	RiSunLine,
+	RiMoonLine,
+} from 'react-icons/ri';
+import { useEffect } from 'react';
+import { themeChange } from 'theme-change';
 
 // TODO: FIX LOGIN PADDING
 const StaticElements = () => {
+	useEffect(() => {
+		themeChange(false);
+		// 👆 false parameter is required for react project
+	}, []);
 	return (
 		<div className='relative'>
 			<Link
@@ -11,8 +23,22 @@ const StaticElements = () => {
 			>
 				TypeRoyale
 			</Link>
-			<button className='absolute bottom-0 p-10 hover:animate-spin text-neutral-content'>
-				<RiSettings3Line className='w-5 h-5' />
+			<button className='absolute bottom-0 p-10 text-neutral-content'>
+				<label class='swap swap-rotate'>
+					<input type='checkbox' />
+					<RiMoonLine
+						class='swap-on fill-current w-5 h-5'
+						data-set-theme='night'
+						data-act-class='ACTIVECLASS'
+					/>
+					<RiSunLine
+						class='swap-off fill-current w-5 h-5'
+						data-set-theme='bumblebee'
+						data-act-class='ACTIVECLASS'
+					/>
+				</label>
+
+				{/* <RiSettings3Line className='w-5 h-5' /> */}
 			</button>
 			<a
 				className='absolute bottom-0 right-0 p-10 hover:animate-wiggle text-neutral-content'
