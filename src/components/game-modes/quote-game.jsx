e22c72810745f428d6937658068655e57d76b8ad
TimTran01 <ttran35112@gmail.com>
2022-08-06 21:53:19 -0400

    created word counter for quote, time, and word games

--- src/components/game-modes/quote-game.jsx
+++ src/components/game-modes/quote-game.jsx
@@ -1,69 +1,87 @@
-import { useState, useEffect, useMemo } from 'react';
-import Button from '../button/button';
-import ClockUp from '../clock-up/clock-up';
-import GameStats from '../game-stats/game-stats';
-import TextBox from '../text-box/text-box';
-import { generateQuote } from '../passage/quote-generation';
+import { useState, useEffect, useMemo } from "react";
+import Button from "../button/button";
+import ClockUp from "../clock-up/clock-up";
+import GameStats from "../game-stats/game-stats";
+import TextBox from "../text-box/text-box";
+import { generateQuote } from "../passage/quote-generation";
 
-const QuoteGame = ({ typed, setTyped, ready, setReady, reset }) => {
-	const [mode, setMode] = useState('all');
-	const [time, setTime] = useState(0);
+const QuoteGame = ({
+  typed,
+  setTyped,
+  ready,
+  setReady,
+  reset,
+  progress,
+  setProgress,
+}) => {
+  const [mode, setMode] = useState("all");
+  const [time, setTime] = useState(0);
+  var passage = useMemo(() => {
+    return generateQuote(mode, reset);
+  }, [mode, reset]);
 
-	var passage = useMemo(() => {
-		return generateQuote(mode, reset);
-	}, [mode, reset]);
+  // ensures text box is focused after mode change
+  useEffect(() => {
+    setReady(false);
+    setProgress(0);
+    setTyped({ val: "", keysPressed: [], done: false });
+    if (document.getElementById("text-box")) {
+      document.getElementById("text-box").focus();
+    }
+  }, [mode, setReady, setTyped, setProgress]);
 
-	// ensures text box is focused after mode change
-	useEffect(() => {
-		setReady(false);
-		setTyped({ val: '', keysPressed: [], done: false });
-		if (document.getElementById('text-box')) {
-			document.getElementById('text-box').focus();
-		}
-	}, [mode, setReady, setTyped]);
-
-	return (
-		<div className='flex flex-col items-center w-screen'>
-			<span className='flex flex-col items-center'>
-				{!typed.done ? (
-					<ClockUp
-						typed={typed}
-						setTyped={setTyped}
-						ready={ready}
-						time={time}
-						setTime={setTime}
-					/>
-				) : (
-					<GameStats typed={typed} gameTime={time} passage={passage.raw} mode="quote"/>
-				)}
-			</span>
-			<span>
-				{!typed.done ? (
-					<TextBox
-						passage={passage}
-						typed={typed}
-						setTyped={setTyped}
-						setReady={setReady}
-						ready={ready}
-						mode="quote"
-					/>
-				) : (
-					<span />
-				)}
-			</span>
-			<span className='btn-group absolute-center'>
-				{['all', 'short', 'med', 'long', 'omega'].map((time, i) => (
-					<Button
-						className='max-width-14'
-						key={i}
-						label={time}
-						mode={mode}
-						setMode={setMode}
-					/>
-				))}
-			</span>
-		</div>
-	);
+  return (
+    <div className='flex flex-col items-center w-screen'>
+      <span className='flex flex-col items-center'>
+        {!typed.done ? (
+          <ClockUp
+            typed={typed}
+            setTyped={setTyped}
+            ready={ready}
+            time={time}
+            setTime={setTime}
+          />
+        ) : (
+          <GameStats
+            typed={typed}
+            gameTime={time}
+            passage={passage.raw}
+            mode='quote'
+          />
+        )}
+      </span>
+      <span>
+        {progress} / {passage.raw.length}
+      </span>
+      <span>
+        {!typed.done ? (
+          <TextBox
+            passage={passage}
+            typed={typed}
+            setTyped={setTyped}
+            setReady={setReady}
+            ready={ready}
+            mode='quote'
+            progress={progress}
+            setProgress={setProgress}
+          />
+        ) : (
+          <span />
+        )}
+      </span>
+      <span className='btn-group absolute-center'>
+        {["all", "short", "med", "long", "omega"].map((time, i) => (
+          <Button
+            className='max-width-14'
+            key={i}
+            label={time}
+            mode={mode}
+            setMode={setMode}
+          />
+        ))}
+      </span>
+    </div>
+  );
 };
 
 export default QuoteGame;
--- src/components/passage/quote-generation.jsx
+++ src/components/passage/quote-generation.jsx
@@ -1,64 +1,68 @@
-import QUOTES from '../../assets/word-lists/quotes.json';
+import QUOTES from "../../assets/word-lists/quotes.json";
 
 export const generateQuote = (chooseLength) => {
-	const LINE_LEN = 50; // play around with value depending on desired text box size
-	const chooseList = QUOTES.quotes;
+  const LINE_LEN = 50; // play around with value depending on desired text box size
+  const chooseList = QUOTES.quotes;
 
-	let quote, quoteList, quoteFilter;
+  let quote, quoteList, quoteFilter;
 
-	switch (chooseLength) {
-		case 'short':
-			quoteFilter = chooseList.filter((curQuote) => {
-				return curQuote.length <= 150;
-			});
-			quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
-			quoteList = quote.split(' ');
-			break;
+  switch (chooseLength) {
+    case "short":
+      quoteFilter = chooseList.filter((curQuote) => {
+        return curQuote.length <= 150;
+      });
+      quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
+      quoteList = quote.split(" ");
+      break;
 
-		case 'med':
-			quoteFilter = chooseList.filter((curQuote) => {
-				return curQuote.length >= 150 && curQuote.length <= 300;
-			});
-			quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
-			quoteList = quote.split(' ');
-			break;
+    case "med":
+      quoteFilter = chooseList.filter((curQuote) => {
+        return curQuote.length >= 150 && curQuote.length <= 300;
+      });
+      quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
+      quoteList = quote.split(" ");
+      break;
 
-		case 'long':
-			quoteFilter = chooseList.filter((curQuote) => {
-				return curQuote.length >= 301 && curQuote.length <= 450;
-			});
-			quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
-			quoteList = quote.split(' ');
-			break;
+    case "long":
+      quoteFilter = chooseList.filter((curQuote) => {
+        return curQuote.length >= 301 && curQuote.length <= 450;
+      });
+      quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
+      quoteList = quote.split(" ");
+      break;
 
-		case 'omega':
-			quoteFilter = chooseList.filter((curQuote) => {
-				return curQuote.length >= 450;
-			});
-			quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
-			quoteList = quote.split(' ');
-			break;
+    case "omega":
+      quoteFilter = chooseList.filter((curQuote) => {
+        return curQuote.length >= 450;
+      });
+      quote = quoteFilter[Math.floor(Math.random() * quoteFilter.length)].text;
+      quoteList = quote.split(" ");
+      break;
 
-		default:
-			quote = chooseList[Math.floor(Math.random() * chooseList.length)].text;
-			quoteList = quote.split(' ');
-	}
+    default:
+      quote = chooseList[Math.floor(Math.random() * chooseList.length)].text;
+      quoteList = quote.split(" ");
+  }
 
-	let charCount = 0;
-	let dispPassage = '';
-	let passage = '';
+  let charCount = 0;
+  let dispPassage = "";
+  let passage = "";
 
-	quoteList.forEach((word) => {
-		charCount += word.length + 1;
-		if (charCount >= LINE_LEN) {
-			dispPassage += word + '\n';
-			passage += word + ' ';
-			charCount = 0;
-		} else {
-			dispPassage += word + ' ';
-			passage += word + ' ';
-		}
-	});
+  quoteList.forEach((word) => {
+    charCount += word.length + 1;
+    if (charCount >= LINE_LEN) {
+      dispPassage += word + "\n";
+      passage += word + " ";
+      charCount = 0;
+    } else {
+      dispPassage += word + " ";
+      passage += word + " ";
+    }
+  });
 
-	return { display: dispPassage.trim(), raw: passage.trim() };
+  return {
+    display: dispPassage.trim(),
+    raw: passage.trim(),
+    length: quoteList.length,
+  };
 };
